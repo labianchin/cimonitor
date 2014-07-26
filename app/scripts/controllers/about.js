6814c8932cbe34e1f0d1a050d494011ffafa646e
Luis Armando Bianchin <luis.armandob@gmail.com>
2014-07-26 13:50:58 -0300

    New angular app with the name cimonitor

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cimonitorApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the cimonitorApp
+ */
+angular.module('cimonitorApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cimonitorApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cimonitorApp
+ */
+angular.module('cimonitorApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
