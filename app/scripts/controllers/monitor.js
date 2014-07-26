6a2990a3e1b78e4ea624f42ecd63813462c22704
Luis Armando Bianchin <luis.armandob@gmail.com>
2014-07-26 18:21:45 -0300

    Basic controller, view style for cimonitor

---
+++ app/scripts/controllers/monitor.js
@@ -0,0 +1,99 @@
+//'use strict';
+
+angular.module('cimonitorApp')
+  .factory('monitorUrl', function($routeParams) {
+    return {
+      url: function(){ 
+        return 'demo/cctray_sample.xml';
+        //return 'ec';
+      },
+      params: function(){
+        var ret = {app: $routeParams.app};
+        return ret;
+      }
+    };
+  });
+
+angular.module('cimonitorApp')
+  .factory('buildFetcherService', function($http, monitorUrl, x2js, _) {
+    var normalizeKeys = function(p) {
+      for (var f in p) {
+        if (p.hasOwnProperty(f)) {
+          var v = p[f];
+          var k = f.slice(1);
+          delete p[f];
+          p[k] = v;
+        }
+      }
+      return p;
+    };
+
+    var obj = {
+      all: [],
+      lastUpdate: '',
+      error: false
+    };
+    var onSuccess = function(data, status, headers, config) {
+      //TODO check if Projects are defined
+      var allProjects = data.Projects.Project
+      obj.all = _.map(allProjects, normalizeKeys);
+      obj.lastUpdate = moment().format('MMM, Do HH:mm:ss');
+      obj.error = false;
+    };
+    var onError = function(data, status, headers, config) {
+      obj.error = true;
+      obj.errorMessage = "Failed to load build status";
+      console.log("Error loading build status, got status " + status + " and data " + data);
+    };
+    var transform2json = function(data){
+      return x2js.xml_str2json(data);
+    };
+
+    obj.update = function() {
+      return $http.get(
+          monitorUrl.url(),
+          {
+            transformResponse: transform2json,
+            params: monitorUrl.params()
+          }
+          )
+        .success(onSuccess)
+        .error(onError);
+    };
+
+    return obj;
+  });
+
+angular.module('cimonitorApp')
+  .factory('spinningService', function(){
+    var obj = {};
+    obj.loading = false;
+    obj.spin = function(promise) {
+      obj.loading = true;
+      var end = function() {
+        obj.loading = false;
+      };
+      promise.then(end, end);
+    }
+    return obj;
+  });
+/**
+ * @ngdoc function
+ * @name ciMonitorApp.controller:MonitorCtrl
+ * @description
+ * # MonitorCtrl
+ * Controller of the ciMonitorApp
+ */
+angular.module('cimonitorApp')
+  .controller('MonitorCtrl', function ($scope, $interval, spinningService, buildFetcherService, moment) {
+    $scope.builds = buildFetcherService;
+    $scope.spinning = spinningService;
+
+    var getBuilds = function () {
+      spinningService.spin(buildFetcherService.update());
+    };
+
+    getBuilds();
+    var timeoutValue = 20000;
+    $interval(getBuilds, timeoutValue);
+  });
